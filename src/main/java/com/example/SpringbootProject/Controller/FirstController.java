58e396a9d41c1a833d1940febb0e9a6db2061794
ju0song <>
2022-05-16 15:51:25 +0900

    no message

---
+++ src/main/java/com/example/SpringbootProject/Controller/FirstController.java
@@ -0,0 +1,24 @@
+package com.example.SpringbootProject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.PublicKey;
+
+//1.Controller 설정
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi") //3. Getmappring하여 접속 url 만들기
+    public String niceToMeetyou(Model model){
+        model.addAttribute("username","ju0");
+        return "greetings";//2.리턴값으로 보여줄 패이지의 이름을 리턴하기
+        //templates/greetings.muctache ->브라우저로 전송
+    }
+    @GetMapping("/bye")
+    public String seeyounext(Model model){
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
+}
