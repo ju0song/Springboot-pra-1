b6941907aeb1fe457f0e040b420f747710f45198
ju0song <cuobalee@naver.com>
2022-05-17 11:23:19 +0900

    form만들기

---
+++ src/main/java/com/example/SpringbootProject/Controller/ArticleController.java
@@ -0,0 +1,19 @@
+package com.example.SpringbootProject.Controller;
+
+import com.example.SpringbootProject.DTO.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("/articles/new")
+    public String newArticleController(){
+        return "articles/new";
+    }
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
+}
